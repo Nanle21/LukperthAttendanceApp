37a0a9f0e4eb7445d55f646f9838092984ed63c2
Nanle Paul <paulnanle611@gmail.com>
2016-10-01 15:17:49 +0100

    Update config.php

--- serverApi/LukperthAtt/config.php
+++ serverApi/LukperthAtt/config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'sunday12');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'lukperth');
 
